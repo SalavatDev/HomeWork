8d9dbd2703a914856c0019c94c9a13d72a9330cc
Salavat <k_salavat@bk.ru>
2022-02-11 15:32:44 +0500

    task 1 complete

---
+++ hw1/task_1.py
@@ -0,0 +1,8 @@
+
+number = int(input('Введите цело число: '))
+float_num = float(input('Введите действительное число: '))
+stroka = input('Введите строку: ')
+
+print(number)
+print(float_num)
+print(stroka)
